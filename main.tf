ecf8504ed8bf12d1e5e32c35cac64858b56cbe27
Your Name <samyt8435@gmail.com>
2025-06-19 04:09:06 +0300

    text

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "aws" {
     region = "us-west-2"
 }
-
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+  sensitive   = false
+}
 resource "aws_vpc" "just_test" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_support   = true
@@ -24,36 +29,80 @@ resource "aws_subnet" "just_test_subnet" {
     }
 }
 
+# resource "aws_instance" "example" {
+#     ami           = "ami-05f9478b4deb8d173"
+#     instance_type = "t2.micro"
+#     subnet_id     = aws_subnet.just_test_subnet.id
+#     vpc_security_group_ids = [aws_security_group.open_all.id]
 
-resource "aws_instance" "example" {
-    ami           = "ami-05f9478b4deb8d173"
-    instance_type = "t2.micro"
-    subnet_id     = aws_subnet.just_test_subnet.id
-    vpc_security_group_ids = [aws_security_group.open_all.id]
+#     user_data = <<-EOF
+#                 #!/bin/bash
+#                 echo "Hello, World" > index.xhtml
+#                 nohup busybox httpd -f -p 8080 &
+#                 EOF
 
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.xhtml
-                nohup busybox httpd -f -p 8080 &
-                EOF
+#     user_data_replace_on_change = true
 
-    user_data_replace_on_change = true
-
-    tags = {
-    Name = "test_instance"
-    }
-}
+#     tags = {
+#     Name = "test_instance"
+#     }
+# }
 
 resource "aws_security_group" "open_all" {
     name        = "open_all"
     description = "Allow all inbound traffic"
     vpc_id = aws_vpc.just_test.id
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
   
 }
+resource "aws_launch_template" "just_test_launch_configuration" {
+  
+    name          = "example-launch-configuration"
+    image_id     = "ami-05f9478b4deb8d173"
+    instance_type = "t2.micro"
+    network_interfaces {
+        security_groups = [aws_security_group.open_all.id]
+    }    
+    user_data =base64encode(<<-EOF
+                echo "Hello, World" > index.xhtml
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    )
+    lifecycle {
+        create_before_destroy = true
+    }
+
+}
+
+resource "aws_autoscaling_group" "just_test_auto_scale" {
+    launch_template {
+        id      = aws_launch_template.just_test_launch_configuration.id
+        version = "$Latest"
+    }
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 1
+    vpc_zone_identifier  = [aws_subnet.just_test_subnet.id]
+    health_check_type    = "EC2"
+    force_delete          = true
+
+    tag {
+        key                 = "Name"
+        value               = "test_instance"
+        propagate_at_launch = true
+    }
+  
+}
+
+# output "instance_public_ip" {
+#     value = aws_instance.example.public_ip
+#     description = "value of the public IP address of the instance"
+  
+# }
+
